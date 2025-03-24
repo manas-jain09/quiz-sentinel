77a66a09c46a04919421bb8a1f10ce58afe29472
Varad Charholikar <103520139+varadc-2304@users.noreply.github.com>
2025-03-24 09:14:17 +0530

    Update QuizPage.tsx

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -6,7 +6,7 @@ const QuizPage = () => {
     <div className="min-h-screen bg-background py-8">
       <div className="container">
         <h1 className="text-3xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-quiz-red to-quiz-red-light">
-          ArenaHQ Quiz
+          ArenaQuiz
         </h1>
         <Quiz />
       </div>
